76d177e0f593696b6b18ebeb6beea56e19fb7dd0
Afaf KELAI <ea_kelai@esi.dz>
2021-04-11 20:07:37 +0100

    Updates

--- Exercises intermediaires.js
+++ Exercises intermediaires.js
@@ -20,6 +20,8 @@ function vowelsNumber(sentence){
 
     return count;
 }
+let str="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
+console.log(lengthSentence(str));
 
 // Methode using loop 
 // program to count the number of vowels in a string
